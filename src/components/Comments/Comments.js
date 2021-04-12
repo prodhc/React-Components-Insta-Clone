63c919410dc0f53d0b2a34bddd8b6cfcc0c4a45f
prodhc <zionywood@gmail.com>
2021-04-12 12:43:19 -0700

    comment and post components worked on

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,10 +4,13 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const { comments } = props.comments;
 
   return (
     <div>
+      {comments.map(comment => {
+        {return <comment comment={comment}/>}
+      })}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
